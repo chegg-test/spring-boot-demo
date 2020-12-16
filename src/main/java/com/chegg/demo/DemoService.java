3442c12bc741535712751ff097040ac743e45318
Venkat Duddu <vduddu@chegg.com>
2020-12-16 15:03:11 -0800

    fix compilation error

--- src/main/java/com/chegg/demo/DemoService.java
+++ src/main/java/com/chegg/demo/DemoService.java
@@ -7,6 +7,12 @@ import org.springframework.stereotype.Service;
 public class DemoService {
 
     public void methodWithLongListOfArguments(int arg0, int arg1, int arg2, int arg3, int arg4, int arg5, int arg6, int arg7, int arg8, int arg9, int arg10) {
+
+        int sum = arg0 + arg1 + arg2 + arg3 + arg4 + arg5 + arg6 + arg7 + arg8 + arg9 + arg10;
+        System.out.println("call to DemoService.methodWithLongListOfArguments()");
+        System.out.println("Sum is: " + sum);
+
+        int sub = arg0 - arg1 - arg2 - arg3 - arg4 - arg5 - arg6 - arg7 - arg8 - arg9 - arg10;
         
     }
 }
