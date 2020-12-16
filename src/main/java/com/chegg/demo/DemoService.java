037535742f2cdc680bb57f6cb2a476c1b858b76f
Venkat Duddu <vduddu@chegg.com>
2020-12-16 14:45:46 -0800

    add class with long list of method arguments

---
+++ src/main/java/com/chegg/demo/DemoService.java
@@ -0,0 +1,10 @@
+package com.chegg.demo;
+
+
+@Service
+public class DemoService {
+
+    public void methodWithLongListOfArguments(int arg0, int arg1, int arg2, int arg3, int arg4, int arg5, int arg6, int arg7, int arg8, int arg9, int arg10) {
+        
+    }
+}
